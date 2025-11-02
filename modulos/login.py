514ce2781180fbe8864a8fb4873d572ad252d8b4
00126222-dev <00126222@uca.edu.sv>
2025-11-01 22:26:48 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -32,7 +32,7 @@ def login():
     contraseña = st.text_input("Contraseña", type="password", key="contrasena_input")
 
     if st.button("Iniciar sesión"):
-        tipo = verificar_usuario(Usuario, Contraseña)
+        tipo = verificar_usuario(usuario, contraseña)
         if tipo:
             st.session_state["usuario"] = usuario
             st.session_state["tipo_usuario"] = tipo
