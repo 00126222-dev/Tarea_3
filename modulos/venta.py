d9e4e5783b2a47c73b5ce4f321e98aa36b395a79
00126222-dev <00126222@uca.edu.sv>
2025-11-01 21:26:13 -0600

    Update venta.py

--- modulos/venta.py
+++ modulos/venta.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def mostrar_venta():
     # Aquí se muestra "Hola Mundo"
-    st.title("Hola Mundo")
+    st.title("Bienvenidos")
     st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
